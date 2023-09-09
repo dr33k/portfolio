e3e34c0f475b38eebf3c9be2e05062f868427942
dr33k <dr33k@users.noreply.github.com>
2023-09-09 04:23:56 +0100

    Added stopwatch experience counter

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function update(counter){
+    var startDate = new Date("2021-01-01");
+    var difference = new Date() - startDate;
+
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    counter.innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
+}
+
+function startCounter(){
+    var counter = document.getElementById("counter");
+    setInterval(()=>update(counter), 1000)
+}
+
+startCounter();
